f87511728627f8c465024eafc6b9f87dd274ea9d
Scott Tully <scott.tully@gmail.com>
2025-10-20 13:04:49 -0400

    Refactor About, Archives, and Lore pages to utilize async rendering for entries, enhancing performance and user experience. Updated entry mapping to streamline content delivery and removed unnecessary pre-rendering logic in Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,4 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://lossdisfunction.com',
   integrations: [mdx(), tailwind({ applyBaseStyles: false })],
-  build: {
-    inlineStylesheets: 'auto',
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            'astro': ['astro'],
-            'mdx': ['@astrojs/mdx'],
-          }
-        }
-      }
-    }
-  },
-  // experimental: {
-  //   contentCollectionCache: true,
-  // }
 });
