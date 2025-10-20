5ebf5738c38e512bb2bcbf9dfc74e1f7fe5cdacc
Scott Tully <scott.tully@gmail.com>
2025-10-20 13:38:29 -0400

    Enhance Astro configuration with new build options for inline stylesheets and manual chunking, and enable experimental content collection caching. Update ColorPulseBackground component styles for improved positioning and layout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,22 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://lossdisfunction.com',
   integrations: [mdx(), tailwind({ applyBaseStyles: false })],
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'astro': ['astro'],
+            'mdx': ['@astrojs/mdx'],
+          }
+        }
+      }
+    }
+  },
+  experimental: {
+    contentCollectionCache: true,
+  }
 });
